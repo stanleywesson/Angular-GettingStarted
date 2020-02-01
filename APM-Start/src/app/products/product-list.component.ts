768e8c4f25c9f0b1665d2ddf70ff04882b548328
stanleywesson <55352889+stanleywesson@users.noreply.github.com>
2020-02-01 21:29:42 +0200

    Class methods and button clicks

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    showImage: boolean = false;
+    imageWidth: number = 50;
+    imageMargin: number = 2;
     products: any[] =
         [
             {
@@ -59,5 +62,9 @@ export class ProductListComponent {
                 "starRating": 4.6,
                 "imageUrl": "assets/images/xbox-controller.png"
             }
-        ]
+        ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
