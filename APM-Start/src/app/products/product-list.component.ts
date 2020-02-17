345c6da08aba55262173ae53de71c0e08c187821
stanleywesson <55352889+stanleywesson@users.noreply.github.com>
2020-02-17 21:31:10 +0200

    Styles done

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent {
